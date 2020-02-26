14232cb81c86180b76f2e4bab057fb2de88ee98a
EmmanuelC1 <emcastillo@csumb.edu>
2020-02-26 15:32:50 -0800

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Emmanuel Castillo on 2/26/20.
+//  Copyright © 2020 Emmanuel Castillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
